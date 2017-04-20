45117c1009b770243cca322985562f0a2e45c74c
Katya Soltan <kattsoltan@gmail.com>
2017-04-20 15:22:23 -0400

    Put in variables to clean up code, seems to work

--- chirp_code.m
+++ chirp_code.m
@@ -1,11 +1,14 @@
 fs = 48000;
-t = 0:(1/fs):3-(1/fs);%0:1/fs:3;
-s = chirp(t,100,1,1000);
+duration = 3;
+f_min = 20;
+f_max = 20000;
+t = 0 : (1/fs) : duration - (1/fs);
+s = chirp(t, f_min, 1, (f_max - f_min) / duration + f_min);
 
-recObj = audiorecorder(48000,16,1);
+recObj = audiorecorder(fs,16,1);
 z = zeros(300);
 before_sound = datestr(now, 'dd-mm-yyyy HH:MM:SS FFF');
-sound([z(1,:), s], 48000);
+sound([z(1,:), s], fs);
 before_record = datestr(now, 'dd-mm-yyyy HH:MM:SS FFF');
 disp('before')
 recordblocking(recObj, 3);
@@ -20,32 +23,35 @@ figure(1)
 clf
 hold on
 plot(t,s)
-t_real = 0:(1/fs):3-(1/fs);
-plot(t_real,y_record)
+% t_real = 0 : (1/fs) : duration - (1/fs);
+plot(t, y_record)
 
+% Fourier Transforms
 Xjw = fft(s.');
 Yjw = fft(y_record);
 Hjw = Yjw ./ Xjw;
-ht = ifft(Hjw);
 
 figure(2)
 clf
-hold on
 magXjw = abs(Xjw);
 magYjw = abs(Yjw);
 magHjw = abs(Hjw);
-plot(linspace(0,0.36,144000*0.36/(2*pi)),magXjw(1:144000*0.36/(2*pi)))
+f_cutoff = 0.36;
+interval = length(magXjw)*f_cutoff/(2*pi);
+
 hold on
-plot(linspace(0,0.36,144000*0.36/(2*pi)),magYjw(1:144000*0.36/(2*pi)))
-plot(linspace(0,0.36,144000*0.36/(2*pi)),magHjw(1:144000*0.36/(2*pi))*1000)
+plot(linspace(0,f_cutoff,interval),magXjw(1:interval))
+plot(linspace(0,f_cutoff,interval),magYjw(1:interval))
+plot(linspace(0,f_cutoff,interval),magHjw(1:interval)*1000)
 ylim([0 1000])
 
 figure(3)
 clf
+% Find impulse response in time
 space_ht = ifft(Hjw);
-plot(t(1:144000*0.36/(2*pi)), abs(space_ht(1:144000*0.36/(2*pi))))
+plot(t(1:interval), abs(space_ht(1:interval)))
 
-
+% Convolution
 [dove,Fs_dove] = audioread('dove.wav');
 dove = dove(:,1);
 z = zeros(100000000,1);
